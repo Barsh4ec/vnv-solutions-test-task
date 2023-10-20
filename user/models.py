b0328028565cd011a1d9fe22431439ea08fc18b6
Taras Borshch <tarasborshch4@gmail.com>
2023-10-20 11:15:53 +0300

    Create django app, create models and migrations

---
+++ user/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class User(AbstractUser):
+    groups = models.ManyToManyField(to=Group, related_name="users")
+
+    def __str__(self) -> str:
+        return self.username
